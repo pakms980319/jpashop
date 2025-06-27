92883e477c2b10a4b20d2c8fe2037972f7800d9d
Pak MinSeok <pakms980319@gmail.com>
2025-06-27 16:58:41 +0900

    주문 조회 V2: 엔티티를 DTO로 변환

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -1,12 +1,17 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -24,5 +29,49 @@ public class OrderApiController {
             order.getOrderItems().forEach(oi -> {oi.getItem().getName();});
         });
         return all;
+    }
+
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAll(new OrderSearch());
+        return orders.stream()
+                .map(OrderDto::new)
+                .toList();
+    }
+
+    @Data
+    public static class OrderDto {
+
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+        private List<OrderItemDto> orderItems;
+
+        public OrderDto(Order o) {
+            this.orderId = o.getId();
+            this.name = o.getMember().getName();
+            this.orderDate = o.getOrderDate();
+            this.orderStatus = o.getStatus();
+            this.address = o.getDelivery().getAddress();
+            this.orderItems = o.getOrderItems()
+                    .stream()
+                    .map(OrderItemDto::new)
+                    .toList();
+        }
+    }
+
+    @Data
+    public static class OrderItemDto {
+        private String name;
+        private int orderPrice;
+        private int count;
+
+        public OrderItemDto(OrderItem oi) {
+            this.name = oi.getItem().getName();
+            this.orderPrice = oi.getOrderPrice();
+            this.count = oi.getCount();
+        }
     }
 }
